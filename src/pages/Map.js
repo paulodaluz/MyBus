d7318d2f9253e6847ae70cc064da9a91e65404e6
Paulo da Luz <paulo.luz@compasso.com.br>
2021-03-21 00:46:43 -0300

    Adding map on initial page

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Button, View, Dimensions, Alert } from 'react-native'
+import MapView, { Marker } from 'react-native-maps'
+import * as Location from 'expo-location'
 
 export default function Map({ navigation, route }) {
+	const [myPosition, seMyposition] = useState(null)
+	const [localizacoes, setLocalizacoes] = useState([])
+
+	const [localicaoAtual, setLocalicaoAtual] = useState({
+			latitude: -28.2612,
+			longitude: -52.4083,
+			latitudeDelta: 0.050,
+			longitudeDelta: 0.050,
+	})
+
+	const getMyPosition = async () => {
+		let { status } = await Location.requestPermissionsAsync()
+
+		if (status !== "granted") {
+				Alert.alert("Permissão de acesso a localização negado!")
+		} else {
+				await Location.getCurrentPositionAsync({})
+					.then(retorno => seMyposition(retorno.coords))
+					.catch(error => Alert.alert("Erro ao acessar o GPS!"))
+		}
+	}
+		useEffect(() => {
+			getMyPosition()
+		}, [])
+
     return (
-        <View style={styles.container}>
+			<View style={styles.container}>
+				<MapView style={styles.mapStyle} initialRegion={localicaoAtual} region={localicaoAtual}>
+					{
+						localizacoes.map((item, key) => <Marker
+									key={key}
+									coordinate={item}
+									title={item.nome}
+							/>)
+					}
 
-            <Text>MAPA AQUI</Text>
+					{myPosition ?
+					<Marker
+							coordinate={myPosition}
+							title={"Onde eu estou!"}
+							// image={orangeMarkerImg}
+						/>
 
-        </View>
-    );
+						: null
+					}
+				</MapView>
+			</View>
+	);
 }
 
 const styles = StyleSheet.create({
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	mapStyle: {
+		width: Dimensions.get('window').width,
+		height: Dimensions.get('window').height
+	},
+})
