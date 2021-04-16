b4c75003ab9b1a42dc4439eaec389ad412837750
Paulo da Luz <paulo.luz@compasso.com.br>
2021-04-16 16:04:21 -0300

    feat(component): creating component Divisor (line to separate itens).

---
+++ src/components/Divisor/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+import { styles } from './style';
+
+const Divisor = () => <View style={styles.divisor} />;
+
+export { Divisor };
+
---
+++ src/components/Divisor/style.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+	divisor: {
+		height: 1,
+		backgroundColor: '#A39BBD',
+		marginTop: '4%',
+		marginBottom: '2%',
+	},
+});
