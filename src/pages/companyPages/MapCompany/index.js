3c168bd0ab6c5ffb444d7f418f49a4405a8e1b6e
Paulo da Luz <paulo.luz@compasso.com.br>
2021-04-17 15:44:35 -0300

    refactor(CompanyMap): refactoring organization and functions.

--- src/pages/companyPages/MapCompany/index.js
+++ src/pages/companyPages/MapCompany/index.js
@@ -1,69 +1,87 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import * as firebase from 'firebase';
+import React, { useEffect, useState } from 'react';
+import { Image, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-import bus_icon from '../../../assets/images/png/bus_icon.png';
-import { getVehiclesInfos, getVehiclesLocalization } from '../../../backend/map/CompanyMap';
+import bus_icon from '../../../assets/icons/png/map/bus_icon.png';
+import { Menu } from '../../../components/Menu';
 import { styles } from './style';
 
 export default function MapCompany({ navigation, route }) {
 	const { user } = route.params;
 
-	const [completeVehicles, setCompleteVehicles] = useState([]);
-	const [vehiclesWithoutlocalization, setVehiclesWithoutlocalization] = useState([]);
+	const [realTimeVehicles, setRealTimeVehicles] = useState([]);
 
-	const [localicaoAtual, setLocalicaoAtual] = useState({
-		latitude: -18.2612,
-		longitude: -50.4083,
-		latitudeDelta: 25,
-		longitudeDelta: 25,
-	});
+	const initialLocalization = {
+		latitude: -28.2612,
+		longitude: -52.4083,
+		latitudeDelta: 0.15,
+		longitudeDelta: 0.15,
+	};
 
-	async function getLocalizationRealTime() {
-		let localizationWithVehicle = await getVehiclesLocalization(vehiclesWithoutlocalization);
-		setCompleteVehicles(localizationWithVehicle);
-		// setTimeout(
-		// 	function() {
-		// 		getLocalizationRealTime()
-		// 	}, 35000);
-	}
+	const getAllLocalizationVehicles = async () => {
+		firebase
+			.database()
+			.ref('/real_time_database')
+			.on('value', (snapchot) => {
+				let allLocalizations = snapchot.val();
+				if (allLocalizations) {
+					buildDadosVehicles(allLocalizations, user.linked_vehicles);
+				}
+			});
+	};
+
+	const buildDadosVehicles = async (allLocalizations, vehiclesPlate) => {
+		const myVehicles = [];
+
+		vehiclesPlate.forEach((vehiclePlate) => {
+			for (let index in allLocalizations) {
+				if (allLocalizations[index][vehiclePlate]) {
+					let vehicle = {
+						registration_plate: vehiclePlate,
+						...allLocalizations[index][vehiclePlate],
+					};
+					myVehicles.push(vehicle);
+				}
+			}
+		});
+		setRealTimeVehicles(myVehicles);
+	};
 
 	useEffect(() => {
-		async function getData() {
-			let vehicleInfos = await getVehiclesInfos(user.linked_vehicles);
-			setVehiclesWithoutlocalization(vehicleInfos);
-			await getLocalizationRealTime();
-		}
-
-		getData();
+		getAllLocalizationVehicles();
 	}, []);
 
 	return (
-		<View style={styles.container}>
-			<MapView style={styles.mapStyle} initialRegion={localicaoAtual} region={localicaoAtual}>
-				{completeVehicles.map((item) => (
+		<View>
+			<MapView
+				style={styles.mapStyle}
+				initialRegion={initialLocalization}
+				region={initialLocalization}
+			>
+				{realTimeVehicles.map((vehicle, key) => (
 					<Marker
-						key={item.registration_plate}
-						coordinate={item.coordinate}
-						title={item.name}
-						// image={bus_icon}
-					/>
+						onPress={() =>
+							navigation.navigate('ListVehicleInfosPassenger', {
+								registrationPlate: vehicle.registration_plate,
+								uid: user.uid,
+								status: vehicle.status,
+							})
+						}
+						key={key}
+						coordinate={{ latitude: vehicle.latitude, longitude: vehicle.longitude }}
+						title={vehicle.registration_plate}
+					>
+						<Image source={bus_icon} style={{ height: 20, width: 20 }} />
+					</Marker>
 				))}
 			</MapView>
-			<View style={styles.menu}>
-				<TouchableOpacity
-					onPress={() => navigation.navigate('CreateNewVehicle', { uid: user.uid })}
-					style={styles.addVehicleButton}
-				>
-					<Text style={styles.buttonText}>CADASTRAR NOVO VEÍCULO</Text>
-				</TouchableOpacity>
 
-				<TouchableOpacity
-					onPress={() => navigation.navigate('SettingsCompany')}
-					style={styles.configButton}
-				>
-					<Text style={styles.buttonText}>CONFIGURAÇÕES</Text>
-				</TouchableOpacity>
-			</View>
+			<Menu
+				onPressFirstButton={() => navigation.navigate('CreateNewVehicle', { uid: user.uid })}
+				textFirstButton={'CADASTRAR NOVO VEÍCULO'}
+				onPressSecondButton={() => navigation.navigate('SettingsCompany')}
+				textSecondButton={'CONFIGURAÇÕES'}
+			/>
 		</View>
 	);
 }
