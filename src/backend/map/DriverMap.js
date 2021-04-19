ca75d1c862bce68ddc96524efa55b00d96388cf4
Paulo da Luz <paulo.luz@compasso.com.br>
2021-04-19 16:21:14 -0300

    refactor: removing unused functions.

--- src/backend/map/DriverMap.js
+++
@@ -1,7 +0,0 @@
-import { registerRealTimeLocalVehicle } from "../../service/MapLocalizationService";
-
-export async function sendLocalizationToFirebase(accessDatabase, infosVehicle) {
-	registerRealTimeLocalVehicle(accessDatabase, infosVehicle);
-
-	return;
-}
